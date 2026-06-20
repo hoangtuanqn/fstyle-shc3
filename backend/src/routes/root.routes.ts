91ab88a0907ca07e2db9398fd8acd53a0ebb5ed1
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-06-21 05:48:30 +0700

    feat: add scoring controller, route, and register in root router

--- backend/src/routes/root.routes.ts
+++ backend/src/routes/root.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import authRouter from '~/routes/auth.routes';
+import scoringRouter from '~/routes/scoring.routes';
 import votingRouter from '~/routes/voting.routes';
 
 const rootRouter = Router();
 
 rootRouter.use('/auth', authRouter);
 rootRouter.use('/voting', votingRouter);
+rootRouter.use('/scoring', scoringRouter);
 
 export default rootRouter;
