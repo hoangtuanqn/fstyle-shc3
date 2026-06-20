f38a7fa30738d9a361560036dd613d6a069d69fd
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-06-20 22:01:47 +0700

    feat(frontend): add app shell and home page

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,34 @@
+import { useScrollReveal } from '../../hooks/useScrollReveal';
+import Hero from '../../components/Hero';
+import MarqueeBand from '../../components/MarqueeBand';
+import About from '../../components/About';
+import Concept from '../../components/Concept';
+import Teams from '../../components/Teams';
+import FCode from '../../components/FCode';
+import ShowcaseNight from '../../components/ShowcaseNight';
+import Timeline from '../../components/Timeline';
+import Partners from '../../components/Partners';
+import Awards from '../../components/Awards';
+import Club from '../../components/Club';
+
+const Home = () => {
+  useScrollReveal();
+
+  return (
+    <>
+      <Hero />
+      <MarqueeBand />
+      <About />
+      <Concept />
+      <Teams />
+      <FCode />
+      <ShowcaseNight />
+      <Timeline />
+      <Partners />
+      <Awards />
+      <Club />
+    </>
+  );
+};
+
+export default Home;
