8ac668b82d677c8bcb1d2cfa322ca06fe9fa4ad2
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-06-22 02:30:07 +0700

    fix: tanstack query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import { Toaster } from 'sonner';
 
@@ -7,26 +8,30 @@ import { store } from '~/store/store';
 import App from './App.tsx';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          style: {
-            background: '#0a0703',
-            border: '1px solid rgba(254,230,34,.18)',
-            color: '#f2ede0',
-            fontFamily: 'Montserrat, sans-serif',
-            fontSize: 13,
-          },
-          classNames: {
-            success: 'toast-success',
-            error: 'toast-error',
-          },
-        }}
-      />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              background: '#0a0703',
+              border: '1px solid rgba(254,230,34,.18)',
+              color: '#f2ede0',
+              fontFamily: 'Montserrat, sans-serif',
+              fontSize: 13,
+            },
+            classNames: {
+              success: 'toast-success',
+              error: 'toast-error',
+            },
+          }}
+        />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 );
