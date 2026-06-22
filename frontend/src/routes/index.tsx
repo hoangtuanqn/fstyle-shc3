8652ea9cfa639e094fad4be77b7468b39de4f596
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-06-23 01:45:07 +0700

    feat: layout route

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,57 @@
+import { Route } from 'react-router';
+
+import { RoleType } from '~/constants/enums';
+import MainLayout from '~/layout/MainLayout';
+import ProtectedRoute from '~/layout/ProtectedRoute';
+import Home from '~/pages/Home';
+import Login from '~/pages/Login';
+import Dashboard from '~/pages/Dashboard';
+import Scoring from '~/pages/Scoring';
+import Leaderboard from '~/pages/Leaderboard';
+import VotingLeaderboard from '~/pages/VotingLeaderboard';
+import Awards from '~/pages/Awards';
+
+export const AppRoutes = () => (
+  <>
+    {/* Public */}
+    <Route element={<MainLayout />}>
+      <Route index element={<Home />} />
+    </Route>
+    <Route path="/login" element={<Login />} />
+
+    {/* Members + BTC FStyle */}
+    <Route element={<ProtectedRoute roleAccess={[RoleType.MEMBER, RoleType.BTC_FSTYLE]} />}>
+      <Route element={<MainLayout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+    </Route>
+
+    {/* Admin + MC */}
+    <Route element={<ProtectedRoute roleAccess={[RoleType.ADMIN, RoleType.MC]} />}>
+      <Route element={<MainLayout />}>
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Route>
+    </Route>
+
+    {/* Admin + BTC FStyle + MC */}
+    <Route element={<ProtectedRoute roleAccess={[RoleType.ADMIN, RoleType.BTC_FSTYLE, RoleType.MC]} />}>
+      <Route element={<MainLayout />}>
+        <Route path="/voting-leaderboard" element={<VotingLeaderboard />} />
+      </Route>
+    </Route>
+
+    {/* Admin + BTC FStyle */}
+    <Route element={<ProtectedRoute roleAccess={[RoleType.ADMIN, RoleType.BTC_FSTYLE]} />}>
+      <Route element={<MainLayout />}>
+        <Route path="/awards" element={<Awards />} />
+      </Route>
+    </Route>
+
+    {/* Admin only */}
+    <Route element={<ProtectedRoute roleAccess={[RoleType.ADMIN]} />}>
+      <Route element={<MainLayout />}>
+        <Route path="/scoring" element={<Scoring />} />
+      </Route>
+    </Route>
+  </>
+);
